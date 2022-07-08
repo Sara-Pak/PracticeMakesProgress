4ef943359a9a57b1bc3452f267afee9a1f8b8880
Sara Pak <94329581+Sara-Pak@users.noreply.github.com>
2022-07-07 23:01:26 -0400

    Add files via upload

---
+++ recursion.php
@@ -0,0 +1,36 @@
+<? <php>
+// quick php program to find factorial by a given number
+// --------------- using recursion ---------------
+
+//recursion method to find the factorial by a given number
+function factorial_using_recursion($n)
+    {
+        if ($n == 0)
+        return 1;
+        // the base case is triggered
+
+        return $n * factorial_using_recursion($n - 1);
+        // recursion calling the function
+    }
+
+    //--------------- using iteration ------------------------
+    
+    //iteration method to find the factorial by a given number
+    function factorial_using_recursion($n)
+        {
+            $res = 1;
+    
+            //iteration method
+            for ($i = 2; $i <= $n; $i++)
+                $res *= $i;
+    
+            return $res;
+    
+        }
+    
+        //driver method
+        $num = 5;
+        print("Factorial of ".$num." using Recursion is: ". factorial_using_recursion(5). "\n");
+        print("Factorial of ".$num." using Iteration is: ". factorial_using_recursion(5). "\n");
+    
+?>
